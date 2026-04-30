0887765911ddb66a9230e86ca697663c5cbf7abf
L-PF <1750775993@qq.com>
2026-04-30 18:07:32 +0800

    final version

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
  #include<stdio.h>
  int main()
  {
- 	printf("Hello,World!\n");
+ 	char input[100];
+	printf("请输入内容：");
+	fgets(input,sizeof(input),stdin);
+	printf("你输入的内容为：%s",input); 
  	return 0;
  }
