7e6b470a90edfa3ea2732e3c61d3bef9e1640612
L-PF <1750775993@qq.com>
2026-05-28 19:05:16 +0800

    pwm控制

--- ex06/ex06.ino
+++ ex06/ex06.ino
@@ -1,25 +1,40 @@
-#define LED1 5
-#define LED2 18
-int fre = 200;
-int re = 8;
-int dutycycle = 0;
+const int LED_A_PIN = 5;      // 灯A引脚
+const int LED_B_PIN = 18;      // 灯B引脚
+const int PWM_FREQ = 5000;     // PWM频率 5kHz
+const int PWM_RESOLUTION = 8;  // 分辨率8位 (0-255)
+
+// 呼吸控制变量
+int brightness = 0;    // 当前亮度 0-255
+int fadeStep = 1;      // 步进值
+int delayTime = 10;     // 延迟时间(ms)
+
 void setup() {
   Serial.begin(115200);
-  ledcAttach(LED1,fre,re);
-  ledcAttach(LED2,fre,re);
+  
+  ledcAttach(LED_A_PIN, PWM_FREQ, PWM_RESOLUTION);
+  ledcAttach(LED_B_PIN, PWM_FREQ, PWM_RESOLUTION);
+  
+  // 初始状态
+  ledcWrite(LED_A_PIN, 0);
+  ledcWrite(LED_B_PIN, 0);
 }
 
 void loop() {
-  for(;dutycycle<=255;dutycycle++)
-  {
-    ledcWrite(LED1,dutycycle);
-    ledcWrite(LED2,255-dutycycle);
-    delay(20);
+
+  brightness += fadeStep;
+  if (brightness <= 0 || brightness >= 255) {
+    fadeStep = -fadeStep;
   }
-  for(;dutycycle>=0;dutycycle--)
-  {
-    ledcWrite(LED1,dutycycle);
-    ledcWrite(LED2,255-dutycycle);
-    delay(20);
-  }
+  
+  ledcWrite(LED_A_PIN, brightness);
+  ledcWrite(LED_B_PIN, 255-brightness);
+   Serial.print("亮度: ");
+  Serial.print(brightness);
+  Serial.print("A: ");
+  Serial.print(brightness);
+  Serial.print("B: ");
+  Serial.println(brightness);
+
+  delay(delayTime);
 }
+
